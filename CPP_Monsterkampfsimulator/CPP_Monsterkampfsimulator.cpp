a8cfcfb7a51df4d1b8300cc7699aa7abee2a4a9b
André <wannefreakless@gmail.com>
2023-03-16 21:49:26 +0100

    ^bla

--- CPP_Monsterkampfsimulator/CPP_Monsterkampfsimulator.cpp
+++ CPP_Monsterkampfsimulator/CPP_Monsterkampfsimulator.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main(int argc, char* argv[])
 {
-    int my_integer_value = 2;
-    float my_float_value = 2.22f;
-
     std::cout << "Hallo Andre" << std::endl;
+    cout << "so geht's auch!"<< endl;
+
+    cout << "type something and I will repeat: ";
+    char sz[256];
+    cin >> sz; //>> is the extraction operator, << is the insertion operator
+    cout << "You typed: " << sz << endl; //repeats only the first word
+
+    cout << "type something and I will repeat: ";
+    cin.getline(sz, 256); //cin.getline() reads a whole line
+    cout << "You typed: " << sz << endl; //repeats the whole line
     return 0;
 }
