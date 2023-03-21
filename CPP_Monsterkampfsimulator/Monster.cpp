e3f678501dfc977786c7087e2c7f0bd2be3b8f72
André <wannefreakless@gmail.com>
2023-03-21 07:34:50 +0100

    edited header Monster class
    added include string and using namespace std in header of Monster class for further usage

--- CPP_Monsterkampfsimulator/Monster.cpp
+++ CPP_Monsterkampfsimulator/Monster.cpp
@@ -1,20 +1,83 @@
 ﻿#include "Monster.h"
 #include <iostream>
-Monster::Monster() //constructor (no return type, same name as class
+Monster::Monster(int _raceChoice) //constructor (no return type, same name as class
 {
 	//constructor code
+	switch (_raceChoice)
+	{
+	case 1:
+		_monsterAttack = 20;
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterHealth = 80;
+		_monsterRace = "DRAGON";
+	case 2:
+		_monsterAttack = 10;
+		_monsterDefense = 10;
+		_monsterSpeed = 10;
+		_monsterHealth = 7;
+		_monsterRace = "GOBLIN";
+	case 3:
+		_monsterAttack = 20;
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterHealth = 80;
+		_monsterRace = "OGRE";
+	case 4:
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterAttack = 20;
+		_monsterHealth = 10;
+		_monsterRace = "ORC";
+	case 5:
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterAttack = 20;
+		_monsterHealth = 10;
+		_monsterRace = "SKELETON";
+	case 6:
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterAttack = 20;
+		_monsterHealth = 10;
+		_monsterRace = "TROLL";
+	case 7:
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterAttack = 20;
+		_monsterHealth = 10;
+		_monsterRace = "VAMPIRE";
+	case 8:
+		_monsterDefense = 20;
+		_monsterSpeed = 10;
+		_monsterAttack = 20;
+		_monsterHealth = 10;
+		_monsterRace = "ZOMBIE";
+	}
 }
-Monster::~Monster() //destructor (no return type, same name as class, prefixed with ~)
+const enum MonsterType 
 {
-	//destructor code
-
-}
+	DRAGON,
+	GOBLIN,
+	OGRE,
+	ORC,
+	SKELETON,
+	TROLL,
+	VAMPIRE,
+	ZOMBIE,
+	MAX_MONSTER_TYPES
+};
+//Monster::~Monster() //destructor (no return type, same name as class, prefixed with ~)
+//{
+//	//destructor code
+//
+//}
 void Monster::CreateMonster()
 {
-	monsterHealth = 100;
+	_monsterHealth = 100;
 };
 
 void Monster::PrintMonster()
 {
-	std::cout << "Monster Health: " << monsterHealth << std::endl;
+	std::cout << "Monster Health: " << _monsterHealth << std::endl;
 };
--- CPP_Monsterkampfsimulator/Monster.h
+++ CPP_Monsterkampfsimulator/Monster.h
@@ -1,75 +1,12 @@
 ﻿#pragma once
-
+#include <string> //also in .cpp?
+using namespace std; //also in .cpp?
 class Monster {
 public:
 	void PrintMonster();
 	void CreateMonster();
-	const enum MonsterType
-	{
-		DRAGON, 
-		GOBLIN, 
-		OGRE, 
-		ORC,
-		SKELETON, 
-		TROLL, 
-		VAMPIRE, 
-		ZOMBIE,
-		MAX_MONSTER_TYPES
-	};
-	Monster(int _raceChoice)
-	{
-		switch (_raceChoice)
-		{
-		case 1:
-			_monsterAttack = 20;
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterHealth = 80;
-			_monsterRace = "DRAGON";
-		case 2:
-			_monsterAttack = 10;
-			_monsterDefense = 10;
-			_monsterSpeed = 10;
-			_monsterHealth = 7;
-			_monsterRace = "GOBLIN";
-		case 3:
-			_monsterAttack = 20;
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterHealth = 80;
-			_monsterRace = "OGRE";
-		case 4:
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterAttack = 20;
-			_monsterHealth = 10;
-			_monsterRace = "ORC";
-		case 5:
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterAttack = 20;
-			_monsterHealth = 10;
-			_monsterRace = "SKELETON";
-		case 6:
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterAttack = 20;
-			_monsterHealth = 10;
-			_monsterRace = "TROLL";
-		case 7:
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterAttack = 20;
-			_monsterHealth = 10;
-			_monsterRace = "VAMPIRE";
-		case 8:
-			_monsterDefense = 20;
-			_monsterSpeed = 10;
-			_monsterAttack = 20;
-			_monsterHealth = 10;
-			_monsterRace = "ZOMBIE";
-		}
-	}
+	const enum MonsterType;
+	Monster(int _raceChoice);
 private:
 	int _monsterHealth{}; //braces initialize to zero
 	int _monsterAttack{};
