906b2795d668fe29900b4f30ac5a4cc12e291d26
André <wannefreakless@gmail.com>
2023-03-30 18:23:48 +0200

    startet funtion for attribute points

--- CPP_Monsterkampfsimulator/HeroCreationMenu.cpp
+++ CPP_Monsterkampfsimulator/HeroCreationMenu.cpp
@@ -5,36 +5,50 @@
 #include "HeroCreationMenu.h"
 #include "HeroClass.h"
 
-void HeroCreationMenu::IncreaseAttribute(HeroClass* const pHeroClass) 
+int HeroCreationMenu::increaseFunction(int fun_mValue, int m_maximumAttributes,
+	int m_minimumAttributes, int m_currentAttriebutes[], char m_input)
 {
-	//right arrow key
-	//increase attribute
-	//if attribute is 1, do nothing
-	if (1) //1 as placeholder
+	// fun_mValue -> the new amount of attribute points
+	// m_maximumAttributes -> the maximum amount of attribute points
+	// m_minimumAttributes -> the minimum amount of attribute points
+	// m_currentAttriebutes -> the current selected attribute in the array of attributes
+	// m_input -> the input from the keyboard
+	while (_AttributePoints != 0)
 	{
-		//increase attribute
-	}
-	else
-	{
-		//do nothing
-	}
-}
-void HeroCreationMenu::DecreaseAttribute(HeroClass* const pHeroClass) 
-{
-	//left arrow key
-	//decrease attribute
-	//if attribute is 1, do nothing
-	if (1)//1 as placeholder
-	{
-		//decrease attribute
-	}
-	else
-	{
-		//do nothing
+		m_input = _getch();
+
+		if (m_input = 72 && index >= 0 && index < 3)//arrowkey up
+		{
+			index++;
+		}
+		if (m_input = 80 && index <= 3 && index > 0)//arrowkey down
+		{
+			index--;
+		}
+
+		if (m_input == 75)//arrowkey left
+		{
+			if (m_currentAttriebutes[index])
+			{
+
+			}
+		}
+		if (m_input == 77)//arrowkey right
+		{
+			if (m_currentAttriebutes[index])
+			{
+
+			}
+		}
+		if (m_input == 13 && m_currentAttriebutes > 0)//enterkey
+		{
+
+		}
+		return m_currentAttriebutes[fun_mValue];
 	}
 }
 
-void HeroCreationMenu::StartHeroCreation(HeroClass* const pHeroClass) 
+void HeroCreationMenu::StartHeroCreation(HeroClass* const pHeroClass)
 {
 	//menu with arrowkey input up and down for choosing attributes
 	//menu with arrowkey input left and right for decreasing or increasing attributes
@@ -42,9 +56,12 @@ void HeroCreationMenu::StartHeroCreation(HeroClass* const pHeroClass)
 	// Warning if attribute points are left, with confirmation and go back
 	// 6.th menu point to go back to main menu
 
-	int _heroHealth = pHeroClass->GetHealth()+1;//give all attributes at least 1 point
-	int _heroAttack = pHeroClass->GetAttack()+1;
-	int _heroDefense = pHeroClass->GetDefense()+1;
-	int _heroSpeed = pHeroClass->GetSpeed()+1;
+	int _heroHealth = pHeroClass->GetHealth() + 1;//give all attributes at least 1 point
+	int _heroAttack = pHeroClass->GetAttack() + 1;
+	int _heroDefense = pHeroClass->GetDefense() + 1;
+	int _heroSpeed = pHeroClass->GetSpeed() + 1;
+	int _heroAttributes[4] = { _heroHealth, _heroAttack, _heroDefense, _heroSpeed };
+
+	auto value = pFunc_mValue(1, _maximumAttributes, _minAttributes, _heroAttributes[index], key);
 
 }
--- CPP_Monsterkampfsimulator/HeroCreationMenu.h
+++ CPP_Monsterkampfsimulator/HeroCreationMenu.h
@@ -4,12 +4,15 @@ class HeroCreationMenu
 {
 public:
 	void StartHeroCreation(HeroClass* const pHeroClass);
-	void IncreaseAttribute(HeroClass* const pHeroClass);
-	void DecreaseAttribute(HeroClass* const pHeroClass);
 private:
 	char key{}; //key input
 	int _maximumAttributes = 200; //maximum amount of attribute points
-	int _attributePoints = 200; //amount of attribute points to spent
-	int _minAttributePoints = 1; //minimum amount of attribute points for each attribute
+	int _AttributePoints = 200; //amount of attribute points to spent
+	int _minAttributes = 1; //minimum amount of attribute points for each attribute
+	int _heroAttributes[4]; //array for the attributes
+	int index;
+	int(*pFunc_mValue)(int,int,int,int,char);
+	int increaseFunction(int pFunc_mValue, int m_maximumAttributes,
+		int m_minAttributes, int m_currentAttributes[], char m_inputChar);
 };
 
