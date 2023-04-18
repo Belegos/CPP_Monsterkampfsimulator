dbf4b6870c054f497f7d3e30ff1ed26562dca533
Andre <andre.biedermann@me.com>
2023-04-18 17:08:08 +0200

    ...

--- CPP_Monsterkampfsimulator/FightingClass.cpp
+++ CPP_Monsterkampfsimulator/FightingClass.cpp
@@ -2,15 +2,17 @@
 
 void FightingClass::InitFight(Commands* const pCommands, HeroClass* pHeroClass)
 {
-
+	int _roundCounter = 1;
 	while (pHeroClass->ModifyHealth() > 0)
 	{
-		int _rndNumber = 1; //for testing
+		int _rndNumber = 8; //for testing
 		while (pHeroClass->ModifyHealth() > 0)
 		{
+			std::cout << ">>>>>> Round " << _roundCounter << " has started. <<<<<<" << std::endl;
 			m_Enemy = SelectEnemy(_rndNumber, m_factory, m_Enemy);
 			StartFight(m_Enemy, pHeroClass);
 			if (pHeroClass->ModifyHealth() <= 0)break;
+			_roundCounter++;
 		}
 		if (pHeroClass->ModifyHealth() <= 0)break;
 
@@ -61,24 +63,36 @@ void FightingClass::StartFight(Monster* m_Enemy, HeroClass* pHeroClass)
 	int _heroAttack = pHeroClass->ModifyAttack() - m_Enemy->ModifyDefense();
 	int _enemyAttack = m_Enemy->ModifyAttack() - pHeroClass->ModifyDefense();
 
-	std::cout << "Der Kampf beginnt!";
+	std::cout << "Der Kampf beginnt!"<< std::endl;
 
 	while (pHeroClass->ModifyHealth() >= 1 || m_Enemy->ModifyHealth() >= 1)
 		if (pHeroClass->ModifySpeed() > m_Enemy->ModifySpeed())
 		{
 			//hero attacks first
 			HeroAttack(pHeroClass, _heroAttack, m_Enemy, _enemyAttack);
-			if (m_Enemy->ModifyHealth() <= 0)break;
+			if (m_Enemy->ModifyHealth() <= 0)
+			{
+				break;
+			}
 			EnemyAttack(pHeroClass, _heroAttack, m_Enemy, _enemyAttack);
-			if (pHeroClass->ModifyHealth() <= 0)break;
+			if (pHeroClass->ModifyHealth() <= 0)
+			{
+				break;
+			}
 		}
 		else
 		{
 			//Enemy attacks first
 			EnemyAttack(pHeroClass, _heroAttack, m_Enemy, _enemyAttack);
-			if (pHeroClass->ModifyHealth() <= 0)break;
+			if (pHeroClass->ModifyHealth() <= 0)
+			{
+				break;
+			}
 			HeroAttack(pHeroClass, _heroAttack, m_Enemy, _enemyAttack);
-			if (m_Enemy->ModifyHealth() <= 0)break;
+			if (m_Enemy->ModifyHealth() <= 0)
+			{
+				break;
+			}
 		}
 }
 
@@ -98,7 +112,7 @@ void FightingClass::HeroAttack(HeroClass* pHeroClass, int _heroAttack, Monster* 
 	}
 	if (pHeroClass->ModifyAttack() - m_Enemy->ModifyDefense() < 1)
 	{
-		m_Enemy->ModifyHealth(m_Enemy->ModifyHealth()-1);
+		m_Enemy->ModifyHealth(m_Enemy->ModifyHealth() - 1);
 		std::cout << "Hero hits Monster for 1 damage." << std::endl;
 		std::cout << "Monster has " << m_Enemy->ModifyHealth() << " left." << std::endl;
 	}
@@ -108,14 +122,14 @@ void FightingClass::EnemyAttack(HeroClass* pHeroClass, int _heroAttack, Monster*
 {
 	if (m_Enemy->ModifyAttack() - pHeroClass->ModifyDefense() >= 1)
 	{
-		pHeroClass->ModifyHealth(pHeroClass->ModifyHealth()-_enemyAttack);
+		pHeroClass->ModifyHealth(pHeroClass->ModifyHealth() - _enemyAttack);
 		std::cout << "Monster hits hero for " << _enemyAttack << " damage." << std::endl;
 		if (pHeroClass->ModifyHealth() < 0) { pHeroClass->ModifyHealth(0); }
 		std::cout << "Hero has " << pHeroClass->ModifyHealth() << " left." << std::endl;
 	}
 	if (m_Enemy->ModifyAttack() - pHeroClass->ModifyDefense() < 1)
 	{
-		pHeroClass->ModifyHealth(pHeroClass->ModifyHealth()-1);
+		pHeroClass->ModifyHealth(pHeroClass->ModifyHealth() - 1);
 		std::cout << "Monster hits Hero for 1 damage." << std::endl;
 		std::cout << "Hero has " << pHeroClass->ModifyHealth() << " left." << std::endl;
 	}
